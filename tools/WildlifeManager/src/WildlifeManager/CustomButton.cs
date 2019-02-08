f657f8b82e914c632a8cd7607dee4d92e5a27fc0
Min Kwan Park <minpar@microsoft.com>
2019-02-07 17:20:40 -0800

    Add missing headers.

--- tools/WildlifeManager/src/WildlifeManager/CustomButton.cs
+++ tools/WildlifeManager/src/WildlifeManager/CustomButton.cs
@@ -1,6 +1,6 @@
-﻿using System;
-// Copyright (c) Microsoft. All rights reserved.
+﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
 using System.Windows.Automation.Peers;
 using System.Windows.Controls;
 using System.Windows.Automation.Provider;
