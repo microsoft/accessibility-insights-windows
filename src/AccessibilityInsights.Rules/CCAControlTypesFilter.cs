d0a047f563e6085c72984b49bcc55f3fe1206a2e
Ahme Abdo Ahmed Abdo Mohamed <ahmohame@microsoft.com>
2019-02-11 23:56:29 -0800

    -- preoggress ring and other refacrtoring

--- src/AccessibilityInsights.Rules/CCAControlTypesFilter.cs
+++ src/AccessibilityInsights.Rules/CCAControlTypesFilter.cs
@@ -1,10 +1,10 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
-using AccessibilityInsights.RulesTest;
-using System;
+using AccessibilityInsights.Rules.PropertyConditions;
 using System.Collections.Generic;
+using System.Reflection;
 
-namespace AccessibilityInsights.RulesTest
+namespace AccessibilityInsights.Rules
 {
     public class CCAControlTypesFilter
     {
@@ -27,17 +27,21 @@ namespace AccessibilityInsights.RulesTest
         }
 
         public void Filter() {
-            var iterator = ControlType.All.Difference(ControlType.Custom).GetEnumerator();
+            var type = typeof(ControlType);
+            var fields = type.GetFields(BindingFlags.GetField | BindingFlags.Public | BindingFlags.Static);
 
-            do
+            foreach (FieldInfo field in fields)
             {
-                MainTypes.Add(iterator.Current);
-            } while (iterator.MoveNext());
+                    MainTypes.Add(((ControlTypeCondition)field.GetValue(field)).ControlType);
+            }
+            MainTypes.Remove(((ControlTypeCondition) ControlType.Custom).ControlType);
         }
 
         public bool Contains(int typeId)
         {
             return MainTypes.Contains(typeId);
         }
-    }
+}
+
+
 }
