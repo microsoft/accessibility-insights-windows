ff0a2d250c7735f06dff4f5002782a52a4fb9d87
DaveTryon <Dave.Tryon@microsoft.com>
2019-02-20 19:57:36 -0800

    User Story 1457390: Add uninstall/install logic to VersionSwitcher. App curently requires elevated privileges throughout its lifetime

---
+++ src/AccessibilityInsights.VersionSwitcher/InstallHelper.cs
@@ -0,0 +1,84 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using Microsoft.Deployment.WindowsInstaller;
+using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+using System.Globalization;
+
+namespace AccessibilityInsights.VersionSwitcher
+{
+    internal static class InstallHelper
+    {
+        internal static bool InstallNewVersion(string msiPath)
+        {
+            try
+            {
+                Trace.TraceInformation("Attempting to install from \"{0}\"", msiPath);
+                Stopwatch stopwatch = Stopwatch.StartNew();
+                Installer.SetInternalUI(InstallUIOptions.Silent);
+                Installer.InstallProduct(msiPath, "");
+                stopwatch.Stop();
+                Trace.TraceInformation("Installed {0} in {1} milliseconds", msiPath, stopwatch.ElapsedMilliseconds);
+                return true;
+            }
+            catch (ArgumentException e)
+            {
+                Trace.TraceError("Caught ArgumentException: " + e.ParamName + " " + e.Message);
+            }
+            catch (Exception e)
+            {
+                Trace.TraceError("Caught Exception: " + e);
+            }
+            return false;
+        }
+
+        internal static bool DeleteOldVersion(string productName)
+        {
+            try
+            {
+                Trace.TraceInformation("Attempting to find product: \"{0}\"", productName);
+                Stopwatch stopwatch = Stopwatch.StartNew();
+                string productId = FindInstalledProductKey(productName).ToString("B", CultureInfo.InvariantCulture);
+                stopwatch.Stop();
+
+                Trace.TraceInformation("Found productId: {0} in {1} milliseconds", productId, stopwatch.ElapsedMilliseconds);
+
+                stopwatch.Restart();
+                Installer.SetInternalUI(InstallUIOptions.Silent);
+                Installer.ConfigureProduct(productId, 0, InstallState.Absent, "");
+                stopwatch.Start();
+                Trace.TraceInformation("Deleted productId: {0} in {1} milliseconds", productId, stopwatch.ElapsedMilliseconds);
+                return true;
+            }
+            catch (ArgumentException e)
+            {
+                Trace.TraceError("Caught ArgumentException: " + e.ParamName + " " + e.Message);
+            }
+            catch (Exception e)
+            {
+                Trace.TraceError("Caught Exception: " + e);
+            }
+            return false;
+        }
+
+        private static Guid FindInstalledProductKey(string productName)
+        {
+            RegistryKey productsKey = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall");
+            {
+                foreach (string keyName in productsKey.GetSubKeyNames())
+                {
+                    RegistryKey subKey = productsKey.OpenSubKey(keyName);
+                    string registryProductName = (string)subKey.GetValue("DisplayName", string.Empty);
+
+                    if (registryProductName.Equals(productName, StringComparison.OrdinalIgnoreCase))
+                    {
+                        return new Guid(keyName);
+                    }
+                }
+            }
+
+            throw new ArgumentException("Unable to locate key for product: " + productName, nameof(productName));
+        }
+    }
+}
--- src/AccessibilityInsights.VersionSwitcher/MainWindow.xaml.cs
+++ src/AccessibilityInsights.VersionSwitcher/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+using System.Diagnostics;
+using System.IO;
 using System.Windows;
 
 namespace AccessibilityInsights.VersionSwitcher
@@ -9,9 +12,45 @@ namespace AccessibilityInsights.VersionSwitcher
     /// </summary>
     public partial class MainWindow : Window
     {
+        const string ProductName = "Accessibility Insights For Windows v1.1";
+
         public MainWindow()
         {
             InitializeComponent();
+            Hide();
+
+            if (TryGetMsiPath(out string msiPath))
+            {
+                InstallHelper.DeleteOldVersion(ProductName);
+                InstallHelper.InstallNewVersion(msiPath);
+            }
+
+            Close();
+        }
+
+        private static bool TryGetMsiPath(out string msiPath)
+        {
+            // Temporary implementation for testing--still need to finalize actual command line
+            string[] args = Environment.GetCommandLineArgs();
+            if (args.Length > 1)
+            {
+                if (File.Exists(args[1]))
+                {
+                    msiPath = args[1];
+                    return true;
+                }
+                else
+                {
+                    Trace.TraceError("Invalid Path: " + args[1]);
+                }
+            }
+            else
+            {
+                Trace.TraceError("No msi path specified!");
+            }
+
+            msiPath = null;
+            return false;
         }
     }
 }
