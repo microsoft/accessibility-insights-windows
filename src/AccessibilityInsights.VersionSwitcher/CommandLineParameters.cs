bddec68d43078bc34de9e1473f2ac06ddc5cb3d4
DaveTryon <Dave.Tryon@microsoft.com>
2019-02-26 11:52:09 -0800

    Add more integration code for VSA

---
+++ src/AccessibilityInsights.VersionSwitcher/CommandLineParameters.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace AccessibilityInsights.VersionSwitcher
+{
+    internal class CommandLineParameters
+    {
+        internal string MsiPath { get; }
+
+        internal string NewRing { get; }
+
+        internal CommandLineParameters(string msiPath, string newRing = null)
+        {
+            MsiPath = msiPath;
+            NewRing = newRing;
+        }
+    }
+}
--- src/AccessibilityInsights.VersionSwitcher/MainWindow.xaml.cs
+++ src/AccessibilityInsights.VersionSwitcher/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using AccessibilityInsights.Extensions.Helpers;
 using System;
 using System.Diagnostics;
 using System.IO;
@@ -16,41 +17,88 @@ namespace AccessibilityInsights.VersionSwitcher
 
         public MainWindow()
         {
-            InitializeComponent();
-            Hide();
-
-            if (TryGetMsiPath(out string msiPath))
+            try
             {
+                InitializeComponent();
+                Hide();
+
+                CommandLineParameters parameters = GetCommandLineParameters();
+                string localFile = DownloadFromUriToLocalFile(parameters);
+                ValidateLocalFile(localFile);
                 InstallHelper.DeleteOldVersion(ProductName);
-                InstallHelper.InstallNewVersion(msiPath);
+                InstallHelper.InstallNewVersion(parameters.MsiPath);
+                UpdateConfigWithNewRing(parameters.NewRing);
+                LaunchInstalledApp();
+            }
+            catch(Exception e)
+            {
+                Trace.TraceError(e.ToString());
             }
 
             Close();
         }
 
-        private static bool TryGetMsiPath(out string msiPath)
+        private static CommandLineParameters GetCommandLineParameters()
         {
             // Temporary implementation for testing--still need to finalize actual command line
             string[] args = Environment.GetCommandLineArgs();
+
+            string msiPath = null;
+            string newRing = null;
+
             if (args.Length > 1)
             {
                 if (File.Exists(args[1]))
                 {
                     msiPath = args[1];
-                    return true;
                 }
                 else
                 {
                     Trace.TraceError("Invalid Path: " + args[1]);
                 }
-            }
-            else
-            {
-                Trace.TraceError("No msi path specified!");
+
+                if (args.Length > 2)
+                {
+                    newRing = args[2];
+                }
+
+                return new CommandLineParameters(msiPath, newRing);
             }
 
-            msiPath = null;
-            return false;
+            string input = string.Join(" | ", args);
+            throw new ArgumentException("Invalid Input: " + input);
+        }
+
+        private string DownloadFromUriToLocalFile(CommandLineParameters parameters)
+        {
+            // TODO - copy from MSI, throw exception on error
+            // Return value is where the local file was written
+            return parameters.MsiPath;
+        }
+
+        private void ValidateLocalFile(string localFile)
+        {
+            using (TrustVerifier verifier = new TrustVerifier(localFile))
+            {
+                if (!verifier.IsVerified)
+                {
+                    // TODO : Better error messaging
+                    throw new ArgumentException("Untrusted file!", nameof(localFile));
+                }
+            }
+        }
+
+        private void UpdateConfigWithNewRing(string newRing)
+        {
+            if (newRing != null)
+            {
+                // TODO : Update local config file
+            }
+        }
+
+        private void LaunchInstalledApp()
+        {
+            // TODO : Launch the app
         }
     }
 }
