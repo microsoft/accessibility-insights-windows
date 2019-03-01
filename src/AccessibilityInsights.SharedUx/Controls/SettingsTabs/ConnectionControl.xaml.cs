07230548f27b9e9a3fc95a2efd42fff9cdf9b830
Ahme Abdo Ahmed Abdo Mohamed <ahmohame@microsoft.com>
2019-02-28 16:23:59 -0800

    -- move the message string the resources file

--- src/AccessibilityInsights.SharedUx/Controls/SettingsTabs/ConnectionControl.xaml.cs
+++ src/AccessibilityInsights.SharedUx/Controls/SettingsTabs/ConnectionControl.xaml.cs
@@ -156,7 +156,7 @@ namespace AccessibilityInsights.SharedUx.Controls.SettingsTabs
                 }
                 else
                 {
-                    MessageDialog.Show("URL format is not valid. Example URL: https://dev.azure.com/fabrikam");
+                    MessageDialog.Show(Properties.Resources.ADO_URL_Fromat_Message);
                 }
             }
         }
