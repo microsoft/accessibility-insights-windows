19d35063ae86a072aaf667dd04add680efe78cd1
John Alkire <john.alkire@microsoft.com>
2019-03-04 13:50:15 -0800

    Add new issue filing interfaces

---
+++ src/AccessibilityInsights.Extensions/Interfaces/IssueReporting/IIssueResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AccessibilityInsights.Extensions.Interfaces.IssueReporting
+{
+    /// <summary>
+    /// Interface for the details displayed on the new issue button after filing is complete
+    /// </summary>
+    public interface IIssueResult
+    {
+        string DisplayText { get; }
+
+        Uri IssueLink { get; }
+    }
+}
