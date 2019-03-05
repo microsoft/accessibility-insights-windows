bf328a9bffeb809c7e4cace0cfd76234a462dc36
Ahme Abdo Ahmed Abdo Mohamed <ahmohame@microsoft.com>
2019-03-05 00:22:43 -0800

    -- [WIP]

---
+++ src/Extensions.GitHub/ConfigurationModel.xaml.cs
@@ -0,0 +1,46 @@
+﻿using AccessibilityInsights.Extensions.Interfaces.IssueReporting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AccessibilityInsights.Extensions.GitHub
+{
+    /// <summary>
+    /// Interaction logic for ConfigurationModel.xaml
+    /// </summary>
+    public partial class ConfigurationModel : IssueConfigurationControl
+    {
+        public ConfigurationModel()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Called when save button clicked.
+        /// </summary>
+        /// <returns>The extension’s new configuration, serialized</returns>
+        public override string OnSave()
+        {
+            return "";
+        }
+
+        /// <summary>
+        /// Called when settings page dismissed
+        /// </summary>
+        public override void OnDismiss()
+        {
+
+        }
+    }
+}
---
+++ src/Extensions.GitHub/IssueRepoter.cs
@@ -0,0 +1,52 @@
+﻿using AccessibilityInsights.Extensions.Interfaces.IssueReporting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessibilityInsights.Extensions.GitHub
+{
+    public class IssueRepoter : IIssueReporting
+    {
+        private IssueRepoter _instance;
+        
+        private IssueRepoter()
+        {
+
+        }
+
+        public IssueRepoter getDeafualtInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new IssueRepoter();
+            }
+            return _instance;
+        }
+
+        public string ServiceName => "GitHub Issues Reporting";
+
+        public Guid StableIdentifier => Guid.NewGuid();
+
+        public bool IsConfigured => throw new NotImplementedException();
+
+        public IEnumerable<byte> Logo => null;
+
+        public string LogoText => "GitHub";
+
+        public IssueConfigurationControl ConfigurationControl => throw new NotImplementedException();
+
+        public Task<IIssueResult> FileIssueAsync(IssueInformation issueInfo)
+        {
+            return new Task<IIssueResult>(()=> {
+                return null;
+            });
+        }
+
+        public Task RestoreConfigurationAsync(string serializedConfig)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
