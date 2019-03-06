7d46142a33f4ef39332afd012352ca5102f13273
Ahme Abdo Ahmed Abdo Mohamed <ahmohame@microsoft.com>
2019-03-06 11:31:32 -0800

    fail build on purpose

--- src/AccessibilityInsights.Extensions.GitHub/IssueFormatter.cs
+++ src/AccessibilityInsights.Extensions.GitHub/IssueFormatter.cs
@@ -1,6 +1,4 @@
-﻿// Copyright (c) Microsoft. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-using AccessibilityInsights.Extensions.Interfaces.IssueReporting;
+﻿using AccessibilityInsights.Extensions.Interfaces.IssueReporting;
 
 namespace AccessibilityInsights.Extensions.GitHub
 {
