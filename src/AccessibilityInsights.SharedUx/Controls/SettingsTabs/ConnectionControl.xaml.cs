2389f591d220e89fabacf76a1129a94f0e7d2b52
John Alkire <john.alkire@microsoft.com>
2019-03-20 14:21:20 -0700

    Fix typo

--- src/AccessibilityInsights.SharedUx/Controls/SettingsTabs/ConnectionControl.xaml.cs
+++ src/AccessibilityInsights.SharedUx/Controls/SettingsTabs/ConnectionControl.xaml.cs
@@ -89,12 +89,12 @@ namespace AccessibilityInsights.SharedUx.Controls.SettingsTabs
             if (issueConfigurationControl.CanSave)
             {
                 configuration.SelectedIssueReporter = selectedIssueReporter.StableIdentifier;
-                string seralizedConfigs = configuration.IssueReporterSerializedConfigs;
+                string serializedConfigs = configuration.IssueReporterSerializedConfigs;
                 Dictionary<Guid, string> configs = new Dictionary<Guid, string>();
 
-                if (seralizedConfigs != null)
+                if (serializedConfigs != null)
                 {
-                    configs = JsonConvert.DeserializeObject<Dictionary<Guid, string>>(seralizedConfigs);
+                    configs = JsonConvert.DeserializeObject<Dictionary<Guid, string>>(serializedConfigs);
                 }
 
                 string newConfigs = issueConfigurationControl.OnSave();
