8e4ef763347fe8adf2eddfccd5b9bbaa32d1e3e1
Bi Wu <biwu@microsoft.com>
2019-03-22 11:54:34 -0700

    added test images and test cases for verifying color contrast analyzer

--- src/AccessibilityInsights.DesktopTests/ColorContrastAnalyzer/ImageTests.cs
+++ src/AccessibilityInsights.DesktopTests/ColorContrastAnalyzer/ImageTests.cs
@@ -38,6 +38,71 @@ namespace AccessibilityInsights.DesktopTests.ColorContrastAnalyzer
             Assert.AreEqual(Confidence.High, result.ConfidenceValue());
         }
 
+        [TestMethod, Timeout(2000)]
+        public void SimplePurpleAndWhiteButton()
+        {
+            var image = LoadFromResources("simple_purple_and_white_button.bmp");
+
+            var result = image.RunColorContrastCalculation();
+
+            Assert.AreEqual(new ColorPair(new CCColor(55, 0, 110), new CCColor(255, 255, 255)),
+                result.GetMostLikelyColorPair());
+
+            Assert.AreEqual(Confidence.Low, result.ConfidenceValue());
+        }
+
+        [TestMethod, Timeout(2000)]
+        public void SimpleBlueAndWhiteText()
+        {
+            var image = LoadFromResources("simple_blue_and_white_text.bmp");
+
+            var result = image.RunColorContrastCalculation();
+
+            Assert.AreEqual(new ColorPair(new CCColor(27, 12, 170), new CCColor(255, 255, 255)),
+                result.GetMostLikelyColorPair());
+
+            Assert.AreEqual(Confidence.Low, result.ConfidenceValue());
+        }
+
+        [TestMethod, Timeout(2000)]
+        public void SimpleGreyAndWhiteTitle()
+        {
+            var image = LoadFromResources("simple_grey_and_white_title.bmp");
+
+            var result = image.RunColorContrastCalculation();
+
+            Assert.AreEqual(new ColorPair(new CCColor(153, 171, 206), new CCColor(255, 255, 255)),
+                result.GetMostLikelyColorPair());
+
+            Assert.AreEqual(Confidence.High, result.ConfidenceValue());
+        }
+
+        [TestMethod, Timeout(2000)]
+        public void SimpleBlackAndWhiteTitle()
+        {
+            var image = LoadFromResources("simple_black_and_white_title.bmp");
+
+            var result = image.RunColorContrastCalculation();
+
+            Assert.AreEqual(new ColorPair(new CCColor(0, 0, 0), new CCColor(255, 255, 219)),
+                result.GetMostLikelyColorPair());
+
+            Assert.AreEqual(Confidence.High, result.ConfidenceValue());
+        }
+
+        [TestMethod, Timeout(2000)]
+        public void SimpleWhiteAndBlackText()
+        {
+            var image = LoadFromResources("simple_white_and_black_text.bmp");
+
+            var result = image.RunColorContrastCalculation();
+
+            Assert.AreEqual(new ColorPair(new CCColor(0, 0, 58), new CCColor(255, 255, 255)),
+                result.GetMostLikelyColorPair());
+
+            Assert.AreEqual(Confidence.High, result.ConfidenceValue());
+        }
+
         /**
          * In this test we are analyzing two similar images. One with text near the bottom of the image
          * and the other with the text near the top. The results should be identical.
