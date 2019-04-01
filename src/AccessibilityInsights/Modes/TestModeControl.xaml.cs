cbabb98468a0ae25d3f3b957b3492a7ecf0f28eb
John Alkire <john.alkire@microsoft.com>
2019-04-01 16:18:49 -0700

    Explanatory comment

--- src/AccessibilityInsights/Modes/TestModeControl.xaml.cs
+++ src/AccessibilityInsights/Modes/TestModeControl.xaml.cs
@@ -190,7 +190,10 @@ namespace AccessibilityInsights.Modes
                                     {
                                         ScreenShotAction.CaptureScreenShot(ecId);
                                         Application.Current.MainWindow.WindowStyle = WindowStyle.SingleBorderWindow;
-                                        Application.Current.MainWindow.Visibility = Visibility.Visible;
+
+                                        // This needs to happen before the call to ctrlAutomatedChecks.SetElement. Otherwise,
+                                        // ctrlAutomatedChecks's checkboxes become out of sync with the highlighter
+                                        Application.Current.MainWindow.Visibility = Visibility.Visible; 
                                     })).Wait();
                                 }
                             }
