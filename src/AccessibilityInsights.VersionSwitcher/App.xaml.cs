759ac6cbe376ade4e634243691d3c88ee986dc65
DaveTryon <Dave.Tryon@microsoft.com>
2019-04-01 15:25:24 -0700

    Add missing licensing header

--- src/AccessibilityInsights.VersionSwitcher/App.xaml.cs
+++ src/AccessibilityInsights.VersionSwitcher/App.xaml.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
 using System.Windows;
 
 namespace AccessibilityInsights.VersionSwitcher
