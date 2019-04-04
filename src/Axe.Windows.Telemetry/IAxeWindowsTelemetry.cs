e5057b6818f663d2eca2f0a8bef42e11ef1b1712
Rob Gallo <rogallo@microsoft.com>
2019-04-04 18:16:48 -0400

    Renaming a parameter to be congruent between the ITelemetry and IAxeWindowsTelemetry interfaces.

--- src/Axe.Windows.Telemetry/IAxeWindowsTelemetry.cs
+++ src/Axe.Windows.Telemetry/IAxeWindowsTelemetry.cs
@@ -12,7 +12,7 @@ namespace Axe.Windows.Telemetry
         /// </summary>
         /// <param name="eventName">The name of the event. Will be ignored if trivial</param>
         /// <param name="properties">The properties to include with the event. Will be ignored if trivial</param>
-        void PublishEvent(string action, IReadOnlyDictionary<string, string> properties);
+        void PublishEvent(string eventName, IReadOnlyDictionary<string, string> properties);
 
         /// <summary>
         /// Report an Exception into the pipeline
