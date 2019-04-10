8b2d1fdb4693929da15181f746b53ecca8b69d61
Karanbir Singh <karansin@microsoft.com>
2019-04-10 16:16:39 -0700

    additional header

--- src/AccessibilityInsights.RulesTest/PropertyConditions/ValuePatternTests.cs
+++ src/AccessibilityInsights.RulesTest/PropertyConditions/ValuePatternTests.cs
@@ -1,4 +1,6 @@
-﻿using Axe.Windows.Core.Bases;
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using Axe.Windows.Core.Bases;
 using Axe.Windows.Rules.PropertyConditions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
