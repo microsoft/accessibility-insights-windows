7de4d1003d81f5a05c022352ef9147dbb8d724eb
DaveTryon <Dave.Tryon@microsoft.com>
2019-04-11 16:01:24 -0700

    and more...

--- tools/scripts/ValidateReleaseInfo.ps1
+++ tools/scripts/ValidateReleaseInfo.ps1
@@ -69,7 +69,7 @@ Add-Type -Path ((Get-Location).Path + '\Octokit.0.32.0\lib\net45\Octokit.dll')
 # Get a new client with an appropriate product header value
 $productHeader = [Octokit.ProductHeaderValue]::new("AIWindows-ReleaseInfoValidation")
 $client = [Octokit.GitHubClient]::new($productHeader)
-$client.Credentials = [Octokit.Credentials]::new($(gitPATX))
+$client.Credentials = [Octokit.Credentials]::new($env:MYPAT)
 
 
 # Get tag names for existing releases
