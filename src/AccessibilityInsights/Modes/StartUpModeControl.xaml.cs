0a636e87c084e6b5cbdc8731397ab6f64c738fb4
DaveTryon <45672944+DaveTryon@users.noreply.github.com>
2019-04-17 16:52:45 -0700

    Update src/AccessibilityInsights/Modes/StartUpModeControl.xaml.cs
    Co-Authored-By: AhmedAbdoOrtiga <45858632+AhmedAbdoOrtiga@users.noreply.github.com>

--- src/AccessibilityInsights/Modes/StartUpModeControl.xaml.cs
+++ src/AccessibilityInsights/Modes/StartUpModeControl.xaml.cs
@@ -77,7 +77,7 @@ namespace AccessibilityInsights.Modes
 
         public StartUpModeControl()
         {
-            this.VersionString =VersionTools.GetAppVersion();
+            this.VersionString = VersionTools.GetAppVersion();
             InitializeComponent();
             // If possible, point to build-specific release notes
             Uri releaseNotesUri = MsiUtilities.GetReleaseNotesUri(new SetupExceptionReporter());
