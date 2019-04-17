07e031a2435e1110d57de018d8ea2d9fd06b15b4
Ahme Abdo Ahmed Abdo Mohamed <ahmohame@microsoft.com>
2019-04-16 19:04:13 -0700

    --test2

---
+++ src/AccessibilityInsights.SharedUx/Misc/AssemblyInfoHelper.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using AccessibilityInsights.SetupLibrary;
+using AccessibilityInsights.SharedUx.Telemetry;
+using System;
+
+namespace AccessibilityInsights.SharedUx.Misc
+{
+    /// <summary>
+    /// Class to pass as an IExceptionReporter when calling into SetupLibrary
+    /// </summary>
+    public class SetupExceptionReporter : IExceptionReporter
+    {
+        /// <summary>
+        /// Implements <see cref="IExceptionReporter.ReportException(Exception)"/>
+        /// </summary>
+        /// <param name="exception">The Exception being reported</param>
+        public void ReportException(Exception exception)
+        {
+            exception.ReportException();
+        }
+    }
+}
