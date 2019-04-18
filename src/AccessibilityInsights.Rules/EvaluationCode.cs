f3f3e6afab6b59a1bc234443f2ba84f9cb1ca13e
RobGallo <43080011+RobGallo@users.noreply.github.com>
2019-04-18 18:13:59 -0400

    Removing extra spaces.

--- src/AccessibilityInsights.Rules/EvaluationCode.cs
+++ src/AccessibilityInsights.Rules/EvaluationCode.cs
@@ -14,12 +14,12 @@ namespace Axe.Windows.Rules
         NotApplicable,
 
         /// <summary>
-        /// The given element met the success criteria  of the rule evaluation.
+        /// The given element met the success criteria of the rule evaluation.
         /// </summary>
         Pass,
 
         /// <summary>
-        /// The given element did not meet the success criteria  of the rule evaluation.
+        /// The given element did not meet the success criteria of the rule evaluation.
         /// </summary>
         Error,
 
@@ -34,7 +34,7 @@ namespace Axe.Windows.Rules
         Note,
 
         /// <summary>
-        /// The given element did not meet the success criteria  of the rule evaluation,
+        /// The given element did not meet the success criteria of the rule evaluation,
         /// but the cause is known to be difficult for developers to fix
         /// (as in issues caused by the platform itself)
         /// or impact to users has been determined to be low.
