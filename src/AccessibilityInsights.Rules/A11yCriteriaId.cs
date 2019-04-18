3cbffff0522ab342d7ba1e4473ade6a06e582469
RobGallo <43080011+RobGallo@users.noreply.github.com>
2019-04-18 11:31:47 -0400

    Enabling XML documentation for the Rules project.
    This will provide intellisense information for users of our NuGet package.
    
    - Updated missing class and method documentation.
    - Fixed invalid formatting in XML
    - Removed a resource file which was not used.

--- src/AccessibilityInsights.Rules/A11yCriteriaId.cs
+++ src/AccessibilityInsights.Rules/A11yCriteriaId.cs
@@ -3,12 +3,35 @@
 
 namespace Axe.Windows.Rules
 {
+    /// <summary>
+    /// Values indicate standards from which rules are derived.
+    /// They map to sections of WCAG and Section 508 compliance documents.
+    /// </summary>
     public enum A11yCriteriaId
     {
+        /// <summary>
+        /// See <a href="https://www.w3.org/TR/UNDERSTANDING-WCAG20/content-structure-separation-programmatic.html">the WCAG documentation</a>
+        /// </summary>
         InfoAndRelationships,
+
+        /// <summary>
+        /// See <a href="https://www.w3.org/TR/UNDERSTANDING-WCAG20/keyboard-operation-keyboard-operable.html">the WCAG documentation</a>
+        /// </summary>
         Keyboard,
+
+        /// <summary>
+        /// See <a href="https://www.w3.org/TR/UNDERSTANDING-WCAG20/ensure-compat-rsv.html">the WCAG documentation</a>
+        /// </summary>
         NameRoleValue,
+
+        /// <summary>
+        /// See <a href="https://www.access-board.gov/guidelines-and-standards/communications-and-it/about-the-ict-refresh/final-rule/single-file-version#502-interoperability-assistive-technology">the Access Board documentation</a>
+        /// </summary>
         ObjectInformation,
+
+        /// <summary>
+        /// See <a href="https://www.access-board.gov/guidelines-and-standards/communications-and-it/about-the-ict-refresh/final-rule/single-file-version#502-interoperability-assistive-technology">the Access Board documentation</a>
+        /// </summary>
         AvailableActions,
     } // class
 } // namespace
--- src/AccessibilityInsights.Rules/EvaluationCode.cs
+++ src/AccessibilityInsights.Rules/EvaluationCode.cs
@@ -8,12 +8,42 @@ namespace Axe.Windows.Rules
     /// </summary>
     public enum EvaluationCode
     {
-        NotApplicable, // given element did not meet the conditions for the rule
+        /// <summary>
+        /// The given element did not meet the conditions for the rule and was not evaluated.
+        /// </summary>
+        NotApplicable,
+
+        /// <summary>
+        /// The given element met the success criteria  of the rule evaluation.
+        /// </summary>
         Pass,
-        Error, // fail
-        Open, // not enough information to make a determination
-        Note, // informational
+
+        /// <summary>
+        /// The given element did not meet the success criteria  of the rule evaluation.
+        /// </summary>
+        Error,
+
+        /// <summary>
+        /// The rule is unable to make a determination, and the element should be investigated.
+        /// </summary>
+        Open,
+
+        /// <summary>
+        /// The rule's description is informational and does not indicate success or failure.
+        /// </summary>
+        Note,
+
+        /// <summary>
+        /// The given element did not meet the success criteria  of the rule evaluation,
+        /// but the cause is known to be difficult for developers to fix
+        /// (as in issues caused by the platform itself)
+        /// or impact to users has been determined to be low.
+        /// </summary>
         Warning,
+
+        /// <summary>
+        /// An error such as an exception was encountered while evaluating the rule.
+        /// </summary>
         RuleExecutionError
     }
 }
