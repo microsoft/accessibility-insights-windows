3b5f5969b986a35866b202bae944691b06cae3e2
John Alkire <4615491+jalkire@users.noreply.github.com>
2019-05-02 15:42:15 -0700

    Remove all duplicate CommonUx controls and resources from DesktopUI (#346)
    * Remove all duplicate CommonUx controls and resources from DesktopUI
    
    * Add SharedResources class to CommonUxComponents for accessing resources. Use it.

---
+++ src/AccessibilityInsights.CommonUxComponents/Utilities/SharedResources.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+using System.Windows;
+using System.Windows.Resources;
+
+namespace AccessibilityInsights.CommonUxComponents.Utilities
+{
+    /// <summary>
+    /// Provides access to resources contained in CommonUxComponents. Both CommonUxComponents as well as
+    /// outside assemblies should go through this class to programmatically access these resources.
+    /// </summary>
+    public static class SharedResources
+    {
+        private static readonly Uri FabricIconFontUri = new Uri(@"pack://application:,,,/AccessibilityInsights.CommonUxComponents;component/Resources/FabMDL2.ttf");
+
+        public static StreamResourceInfo FabricIconFontResource => Application.GetResourceStream(FabricIconFontUri);
+    }
+}
