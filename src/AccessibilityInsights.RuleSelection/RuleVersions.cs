9aed9337ab6911fa319247eadd2bf88d1d98b8ac
Karanbir Singh <karansin@microsoft.com>
2019-05-08 16:57:30 -0700

    Deleted old files, unit tests pass, added nuget reference in WebApiHost

--- src/AccessibilityInsights.RuleSelection/RuleVersions.cs
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-namespace Axe.Windows.RuleSelection
-{
-    static class RuleVersions
-    {
-        public const string Version = "1.0";
-    }
-}
