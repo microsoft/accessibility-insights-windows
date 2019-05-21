dc293ba3a42586712c41624b9b7bd394e8e51571
Karan <karansin@microsoft.com>
2019-05-21 16:52:25 -0700

    Fix for E2E test in signed case and if telemetry dialog shows (#370)
    The E2E test as-is had some issues which are resolved by this change. This change includes 1) modifying the parameters passed to Automation commands in order to ensure the calls succeed 2) filtering out UI tests in the signed build because the WebAppDriver assemblies aren't strong-named, and 3) Closing the telemetry dialog if it is not open in the UI test to ensure we can get to the Getting Started page

--- src/UITests/GettingStartedPage.cs
+++ src/UITests/GettingStartedPage.cs
@@ -1,8 +1,8 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using Axe.Windows.Automation;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
-using Axe.Windows.Automation;
 
 namespace UITests
 {
@@ -10,23 +10,37 @@ namespace UITests
     public class GettingStartedPage : AIWinSession
     {
         [TestMethod]
+        [TestCategory("NoStrongName")]
         public void VerifyGettingStartedTitle()
         {
             Assert.AreEqual("Accessibility Insights for Windows - ", session.Title);
         }
 
         [TestMethod]
+        [TestCategory("NoStrongName")]
         public void VerifyAccessibility()
         {
             var result = SnapshotCommand.Execute(new Dictionary<string, string>
             {
                 { CommandConstStrings.TargetProcessId, testAppProcessId.ToString() },
+                { CommandConstStrings.OutputFile, "GettingStartedPage" },
+                { CommandConstStrings.OutputFileFormat, "a11ytest" }
             });
-            Assert.AreEqual(0, result.ScanResultsPassedCount);
+            Assert.AreEqual(0, result.ScanResultsFailedCount);
         }
 
         [ClassInitialize]
-        public static void ClassInitialize(TestContext context) => Setup(context);
+        public static void ClassInitialize(TestContext context)
+        {
+            Setup(context);
+
+            // Close telemetry dialog if open
+            var elements = session.FindElementsByXPath("//Button[@Name=\"OK\"]");
+            if (elements.Count > 0)
+            {
+                elements[0].Click();
+            }
+        }
 
         [ClassCleanup]
         public static void ClassCleanup() => TearDown();
