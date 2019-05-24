94a9ff5bd24f6402c27205995146d10a37d06920
John Alkire <john.alkire@microsoft.com>
2019-05-24 14:51:38 -0700

    Change UpdateDialog to only be topmost when MainWindow is topmost instead of always.

--- src/AccessibilityInsights/Dialogs/UpdateDialog.xaml.cs
+++ src/AccessibilityInsights/Dialogs/UpdateDialog.xaml.cs
@@ -1,7 +1,6 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 using AccessibilityInsights.CommonUxComponents.Dialogs;
-using AccessibilityInsights.SharedUx.Dialogs;
 using AccessibilityInsights.SharedUx.Telemetry;
 using System;
 using System.Windows;
@@ -23,6 +22,7 @@ namespace AccessibilityInsights.Dialogs
         {
             InitializeComponent();
             this.ReleaseNotesUri = releaseNotesUri;
+            Topmost = App.Current.MainWindow.Topmost;
         }
 
         private void UpdateNow_Click(object sender, RoutedEventArgs e)
