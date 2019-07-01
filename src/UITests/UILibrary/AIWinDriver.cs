e405bdec22f52cbedd9211c28ba3ea970985365c
John Alkire <john.alkire@microsoft.com>
2019-07-01 09:17:10 -0700

    Add copyright headers

--- src/UITests/UILibrary/AIWinDriver.cs
+++ src/UITests/UILibrary/AIWinDriver.cs
@@ -1,7 +1,7 @@
-﻿using Axe.Windows.Automation;
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using Axe.Windows.Automation;
 using OpenQA.Selenium.Appium.Windows;
-using System;
-using System.Collections.Generic;
 
 namespace UITests.UILibrary
 {
