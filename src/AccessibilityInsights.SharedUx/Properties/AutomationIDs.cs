456784aadcc771479bb2e167bf85959614982457
John Alkire <john.alkire@microsoft.com>
2019-07-02 16:22:13 -0700

    Add copyright headers

--- src/AccessibilityInsights.SharedUx/Properties/AutomationIDs.cs
+++ src/AccessibilityInsights.SharedUx/Properties/AutomationIDs.cs
@@ -1,4 +1,6 @@
-﻿namespace AccessibilityInsights.SharedUx.Properties
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+namespace AccessibilityInsights.SharedUx.Properties
 {
     public static class AutomationIDs
     {
--- src/UITests/Utilities/ExtenstionMethods.cs
+++ src/UITests/Utilities/ExtenstionMethods.cs
@@ -1,4 +1,6 @@
-﻿using OpenQA.Selenium.Appium.Windows;
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using OpenQA.Selenium.Appium.Windows;
 
 namespace UITests.Utilities
 {
