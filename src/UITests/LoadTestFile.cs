2059a579f0bfe9ea973ccb2077f2a2b42cb316e2
John Alkire <4615491+jalkire@users.noreply.github.com>
2019-07-09 16:14:46 -0700

    Significantly improve LoadTestFile UITest coverage (#421)

--- src/UITests/LoadTestFile.cs
+++ src/UITests/LoadTestFile.cs
@@ -1,7 +1,13 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using AccessibilityInsights.SharedUx.Properties;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 
 namespace UITests
@@ -12,11 +18,11 @@ namespace UITests
     [TestClass]
     public class LoadTestFile : AIWinSession
     {
-        static readonly string TestFileName = "WildlifeManagerTest.a11ytest";
-        static readonly string TestFilePath = $"{Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)}\\TestFiles";
+        const string TestFileName = "WildlifeManagerTest.a11ytest";
+        readonly string TestFilePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "TestFiles");
 
         /// <summary>
-        /// The entry point for this test scenario. Every TestMethod  will restart ai-win, so
+        /// The entry point for this test scenario. Every TestMethod will restart ai-win, so
         /// we want to use them sparingly.
         /// </summary>
         [TestMethod]
@@ -24,8 +30,10 @@ namespace UITests
         [TestCategory("UITest")]
         public void LoadTestFileTests()
         {
-            ScanResultsInUIATreePage();
             ScanAutomatedChecks();
+            ScanResultsInUIATreePage();
+            ValidateAutomatedChecks();
+            ValidateResultsInUIATree();
         }
 
         private void ScanResultsInUIATreePage()
@@ -42,6 +50,122 @@ namespace UITests
         {
             var issueCount = driver.ScanAIWin(TestContext, "AutomatedChecks");
             Assert.AreEqual(0, issueCount);
+        }
+
+        private void ValidateFirstSelectedElement()
+        {
+            ValidateFirstResults();
+            ValidateFirstDetails();
+            ValidateFirstTree();
+        }
+
+        private void ValidateFirstResults()
+        {
+            var resultsList = driver.FindElementByAccessibilityId(AutomationIDs.ScannerResultsDetailsListView);
+            var resultsFailedOnly = resultsList.FindElementsByClassName("ListViewItem");
+            driver.FindElementByAccessibilityId(AutomationIDs.ScannerResultsShowAllButton).Click();
+            var resultsAll = resultsList.FindElementsByClassName("ListViewItem");
+            var fixFollowTb = driver.FindElementByAccessibilityId(AutomationIDs.ScannerResultsFixFollowingTextBox);
+
+            Assert.IsFalse(string.IsNullOrEmpty(fixFollowTb.Text));
+            Assert.AreEqual(2, resultsFailedOnly.Count);
+            Assert.AreEqual(28, resultsAll.Count);
+
+            resultsAll[0].SendKeys(Keys.Control + Keys.Tab);
+        }
+
+        private void ValidateFirstDetails()
+        {
+            var tree = driver.FindElementByAccessibilityId(AutomationIDs.HierarchyControlUIATreeView);
+            var nodes = tree.FindElementsByClassName("TreeViewItem");
+            var props = driver.FindElementByAccessibilityId(AutomationIDs.SnapshotModeControl).FindElementsByClassName("DataGridRow");
+            props[0].Click();
+            var patterns = GetPatternsNodes(AutomationIDs.SnapshotModeControl, nodes);
+
+            Assert.AreEqual("InvokePattern", patterns.First().Text);
+            Assert.AreEqual("Name, Ok", props[0].Text);
+            Assert.AreEqual(3, patterns.Count());
+            Assert.AreEqual(10, props.Count);
+        }
+
+        private void ValidateFirstTree()
+        {
+            var tree = driver.FindElementByAccessibilityId(AutomationIDs.HierarchyControlUIATreeView);
+            var nodes = tree.FindElementsByClassName("TreeViewItem");
+            nodes.First().SendKeys(Keys.Left);
+
+            Assert.AreEqual(16, nodes.Count);
+            Assert.AreEqual("pane 'Desktop 1' has failed test results in descendants.", nodes.First().Text);
+        }
+
+        private void ValidateRootElement()
+        {
+            ValidateRootDetails();
+            ValidateRootResults();
+        }
+
+        private void ValidateRootDetails()
+        {
+            var props = driver.FindElementByAccessibilityId(AutomationIDs.SnapshotModeControl).FindElementsByClassName("DataGridRow");
+            props[0].Click();
+            var text = props[0].Text;
+            var tree = driver.FindElementByAccessibilityId(AutomationIDs.HierarchyControlUIATreeView);
+            var nodes = tree.FindElementsByClassName("TreeViewItem");
+            var patterns = GetPatternsNodes(AutomationIDs.SnapshotModeControl, nodes);
+
+            Assert.AreEqual("Name, Desktop 1", text);
+            Assert.AreEqual(10, patterns.Count());
+            Assert.AreEqual("LegacyIAccessiblePattern", patterns.First().Text);
+            Assert.AreEqual(10, props.Count);
+
+            // since we've already got an element to use SendKeys, we
+            // switch to the results tab in prepartion for next test
+            patterns.Last().SendKeys(Keys.Control + Keys.Tab);
+        }
+
+        private void ValidateRootResults()
+        {
+            var resultsList = driver.FindElementByAccessibilityId(AutomationIDs.ScannerResultsDetailsListView);
+            var results = resultsList.FindElementsByClassName("ListViewItem");
+            var fixFollowTb = driver.FindElementByAccessibilityId(AutomationIDs.ScannerResultsFixFollowingTextBox);
+
+            Assert.IsTrue(string.IsNullOrEmpty(fixFollowTb.Text));
+            Assert.AreEqual(0, results.Count);
+        }
+
+        private void ValidateResultsInUIATree()
+        {
+            ValidateFirstSelectedElement();
+            ValidateRootElement();
+        }
+
+        private void ValidateAutomatedChecks()
+        {
+            driver.FindElementByAccessibilityId(AutomationIDs.AutomatedChecksExpandAllButton).Click();
+            var resultsGrid = driver.FindElementByAccessibilityId(AutomationIDs.AutomatedChecksResultsListView);
+            var results = resultsGrid.FindElementsByClassName("ListViewItem");
+            var resultsText = driver.FindElementByAccessibilityId(AutomationIDs.AutomatedChecksResultsTextBlock).Text;
+            var resultCount = int.Parse(resultsText.Split()[0]);
+
+            Assert.AreEqual(12, resultCount);
+            Assert.AreEqual(resultCount, results.Count);
+
+            results[0].FindElementByClassName("Button").Click();
+        }
+
+        private IEnumerable<AppiumWebElement> GetPatternsNodes(string parentId, ReadOnlyCollection<AppiumWebElement> nonPatternNodes)
+        {
+            var parent = driver.FindElementByAccessibilityId(parentId);
+            var allnodes = parent.FindElementsByClassName("TreeViewItem");
+            var patterns = allnodes.Except(nonPatternNodes);
+
+            foreach (var pattern in patterns)
+            {
+                pattern.SendKeys(Keys.Right);
+            }
+
+            allnodes = parent.FindElementsByClassName("TreeViewItem");
+            return allnodes.Except(nonPatternNodes);
         }
 
         [TestInitialize]
