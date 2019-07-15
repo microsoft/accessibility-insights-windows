f311ea5c0d5126b78ac317666e9cacf1c18157a7
Dave Tryon <dtryon@microsoft.com>
2019-07-15 13:44:08 -0700

    Update Luminance Calculations

--- src/AccessibilityInsights.SharedUx/ViewModels/ColorContrastViewModel.cs
+++ src/AccessibilityInsights.SharedUx/ViewModels/ColorContrastViewModel.cs
@@ -175,9 +175,9 @@ namespace AccessibilityInsights.SharedUx.ViewModels
             var gSRGB = c.G / 255.0;
             var bSRGB = c.B / 255.0;
 
-            var r = rSRGB <= 0.03928 ? rSRGB / 12.92 : Math.Pow(((rSRGB + 0.055) / 1.055), 2.4);
-            var g = gSRGB <= 0.03928 ? gSRGB / 12.92 : Math.Pow(((gSRGB + 0.055) / 1.055), 2.4);
-            var b = bSRGB <= 0.03928 ? bSRGB / 12.92 : Math.Pow(((bSRGB + 0.055) / 1.055), 2.4);
+            var r = rSRGB <= 0.04045 ? rSRGB / 12.92 : Math.Pow(((rSRGB + 0.055) / 1.055), 2.4);
+            var g = gSRGB <= 0.04045 ? gSRGB / 12.92 : Math.Pow(((gSRGB + 0.055) / 1.055), 2.4);
+            var b = bSRGB <= 0.04045 ? bSRGB / 12.92 : Math.Pow(((bSRGB + 0.055) / 1.055), 2.4);
             return 0.2126 * r + 0.7152 * g + 0.0722 * b;
         }
     }
