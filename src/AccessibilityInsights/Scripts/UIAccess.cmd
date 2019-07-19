6728e9f657fbef76b37fe549f2fb4ded119ed763
DaveTryon <Dave.Tryon@microsoft.com>
2019-07-19 10:53:15 -0700

    Remove extra space at the beginning of each line of UIAccess.cmd

--- src/AccessibilityInsights/Scripts/UIAccess.cmd
+++ src/AccessibilityInsights/Scripts/UIAccess.cmd
@@ -1,42 +1,41 @@
 @echo off
- if /i '%1' == 'Enable' goto Confirm
- if /i '%1' == 'Disable' goto Confirm
+if /i '%1' == 'Enable' goto Confirm
+if /i '%1' == 'Disable' goto Confirm
+
+:Usage
+echo.
+echo UIAccess.cmd
+echo.
+echo This script will set the UIAccess status of Accessibilty Insights for Windows.
+echo UIAccess support is disabled by default. This script must be run from an
+echo administrative command prompt.
+echo.
+echo Usage: UIAccess.cmd ^<action^>
+echo.
+echo Where ^<action^> is one of:
+echo   ENABLE  (to enable UIAccess support)
+echo   DISABLE (to disable UIAccess support)
+echo.
+goto :eof
+
+:Confirm
+echo.
+echo CAUTION! You should only change the UIAccess state if you understand the
+echo          potential security implications!
+echo.
+choice /m "Are you sure you want to %1 UIAccess support?"
+if ErrorLevel 1 goto UpdateManifest
  
- :Usage
- echo.
- echo UIAccess.cmd
- echo.
- echo This script will set the UIAccess status of Accessibilty Insights for Windows.
- echo UIAccess support is disabled by default. This script must be run from an
- echo administrative command prompt.
- echo.
- echo Usage: UIAccess.cmd ^<action^>
- echo.
- echo Where ^<action^> is one of:
- echo   ENABLE  (to enable UIAccess support)
- echo   DISABLE (to disable UIAccess support)
- echo.
- goto :eof
- 
- :Confirm
- echo.
- echo CAUTION! You should only change the UIAccess state if you understand the
- echo          potential security implications!
- echo.
- choice /m "Are you sure you want to %1 UIAccess support?"
- if ErrorLevel 1 goto UpdateManifest
- 
- :UserCanceled
- echo.
- echo Operation was canceled.
- echo.
- goto :eof
- 
- :UpdateManifest
- rem DO NOT use copy here, as we also need to update the file's last write time
- type UIAccess_%1d.manifest > AccessibilityInsights.exe.manifest
- echo.
- echo Update complete. Changes will take effect when Accessibilty Insights for
- echo Windows is next started.
- echo.
- 
+:UserCanceled
+echo.
+echo Operation was canceled.
+echo.
+goto :eof
+
+:UpdateManifest
+rem DO NOT use copy here, as we also need to update the file's last write time
+type UIAccess_%1d.manifest > AccessibilityInsights.exe.manifest
+echo.
+echo Update complete. Changes will take effect when Accessibilty Insights for
+echo Windows is next started.
+echo.
