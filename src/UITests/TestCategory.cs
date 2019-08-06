79e9600f1bc76a8626ad82e2e6a7c0647dc8ef8c
John Alkire <john.alkire@microsoft.com>
2019-08-06 16:05:19 -0700

    Add copyright header

--- src/UITests/TestCategory.cs
+++ src/UITests/TestCategory.cs
@@ -1,4 +1,6 @@
-﻿namespace UITests
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+namespace UITests
 {
     public static class TestCategory
     {
