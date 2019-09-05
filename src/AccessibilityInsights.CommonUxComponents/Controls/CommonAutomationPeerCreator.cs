879acbef2d7bcb412f0ba8df4d05ebde233c8a3e
Karanbir Singh <karansin@microsoft.com>
2019-09-05 10:54:48 -0700

    header update

--- src/AccessibilityInsights.CommonUxComponents/Controls/CommonAutomationPeerCreator.cs
+++ src/AccessibilityInsights.CommonUxComponents/Controls/CommonAutomationPeerCreator.cs
@@ -1,5 +1,5 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.using System.Windows.Automation.Peers;
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
 using System.Windows.Automation.Peers;
 using System.Windows.Controls;
 
