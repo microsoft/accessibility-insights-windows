1aa7df3d09a2219eae853559e25d5e50500bac45
John Alkire <john.alkire@microsoft.com>
2019-09-30 09:18:12 -0700

    Update CustomExpander with comment + more precise behavior

--- src/AccessibilityInsights.SharedUx/Controls/CustomControls/CustomExpander.cs
+++ src/AccessibilityInsights.SharedUx/Controls/CustomControls/CustomExpander.cs
@@ -10,12 +10,19 @@ namespace AccessibilityInsights.SharedUx.Controls.CustomControls
             return new CustomExpanderAutomationPeer(this);
         }
     }
+
     public class CustomExpanderAutomationPeer : ExpanderAutomationPeer
     {
-        public CustomExpanderAutomationPeer(Expander owner)
-            : base(owner)
-        {
-        }
-        override protected bool HasKeyboardFocusCore() => true;
+        GroupItem parentGroupItem;
+
+        public CustomExpanderAutomationPeer(Expander owner) : base(owner) => parentGroupItem = owner?.TemplatedParent as GroupItem;
+
+        /// <summary>
+        /// In .NET 4.8 grouped ListViews, the GroupItem's HasKeyboardFocus value is set based on its child expander's
+        /// HasKeyboardFocus value, not its own IsFocused value. In our AutomatedChecks use case, this breaks ATs'
+        /// readings of the ListView group headers. By setting the child expander's HasKeyboardFocus value here,
+        /// we get the desired value in its parent GroupItem's value, and ATs fuction properly again.
+        /// </summary>
+        override protected bool HasKeyboardFocusCore() => parentGroupItem?.IsFocused ?? base.HasKeyboardFocusCore();
     }
 }
