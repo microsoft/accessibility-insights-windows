a82dc9f58a409b121e42a66414c06c4890f2027f
Dave Tryon <dtryon@microsoft.com>
2020-01-10 14:40:49 -0800

    Port IsVisibleLocation from Axe.Windows to AIWin

--- src/AccessibilityInsights.SharedUx/Utilities/ExtensionMethods.cs
+++ src/AccessibilityInsights.SharedUx/Utilities/ExtensionMethods.cs
@@ -289,5 +289,19 @@ namespace AccessibilityInsights.SharedUx.Utilities
             NativeMethods.DeleteObject(hbmp);
             return result;
         }
+
+        /// <summary>
+        /// Check whether p is fully visible based on current screen size
+        /// </summary>
+        /// <param name="p"></param>
+        /// <param name="c"></param>
+        /// <param name="margin"></param>
+        /// <returns></returns>
+        internal static bool IsVisibleLocation(this Rectangle p)
+        {
+            return (from s in System.Windows.Forms.Screen.AllScreens
+                    where s.Bounds.Contains(p)
+                    select s).Any();
+        }
     }
 }
