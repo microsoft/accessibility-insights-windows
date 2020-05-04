0a4f135f4d1111cfa2c47dbf998c28e8bd2da3a6
Dave Tryon <dtryon@microsoft.com>
2020-05-04 14:36:26 -0700

    Remove superfluous using statements

--- src/AccessibilityInsights.SharedUx/Controls/CustomControls/CustomGridView.cs
+++ src/AccessibilityInsights.SharedUx/Controls/CustomControls/CustomGridView.cs
@@ -1,9 +1,7 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
-using System;
 using System.Windows.Automation.Peers;
 using System.Windows.Controls;
-using System.Windows.Media;
 
 namespace AccessibilityInsights.SharedUx.Controls.CustomControls
 {
