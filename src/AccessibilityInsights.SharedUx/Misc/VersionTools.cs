19735d9bd314d1a69d81d8af138f8737d2ac4280
DaveTryon <45672944+DaveTryon@users.noreply.github.com>
2020-05-14 16:28:47 -0700

    Update the welcome screen (#790)

--- src/AccessibilityInsights.SharedUx/Misc/VersionTools.cs
+++ src/AccessibilityInsights.SharedUx/Misc/VersionTools.cs
@@ -1,7 +1,9 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 using Axe.Windows.Core.Misc;
+using System;
 using System.Diagnostics;
+using System.Globalization;
 using System.Reflection;
 
 namespace AccessibilityInsights.SharedUx.Misc
@@ -16,6 +18,30 @@ namespace AccessibilityInsights.SharedUx.Misc
         {
             string fileVersion = FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).FileVersion;
             return fileVersion;
+        }
+
+        /// <summary>
+        /// Return the label for use in UI
+        /// </summary>
+        public static string AppVersionLabel => string.Format(CultureInfo.InvariantCulture,
+            Properties.Resources.VersionLinkContentFormat, GetAppVersion());
+
+        /// <summary>
+        /// Provides a Uri to the release notes for this version
+        /// </summary>
+        public static Uri AppVersionUri
+        {
+            get
+            {
+                string compressedVersion = GetAppVersion();
+                Version version = Version.Parse(compressedVersion);
+                return new Uri(string.Format(CultureInfo.InvariantCulture,
+                    Properties.Resources.VersionLinkFormat,
+                    version.Major,
+                    version.Minor,
+                    version.Build,
+                    version.Revision));
+            }
         }
 
         public static string GetAxeVersion() => PackageInfo.InformationalVersion;
