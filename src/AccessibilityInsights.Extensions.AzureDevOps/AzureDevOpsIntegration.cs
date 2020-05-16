ad8d414b1d154b4221ece480f0ce17af412cd388
Dave Tryon <dtryon@microsoft.com>
2020-05-16 07:46:16 -0700

    Remove unneeded using block

--- src/AccessibilityInsights.Extensions.AzureDevOps/AzureDevOpsIntegration.cs
+++ src/AccessibilityInsights.Extensions.AzureDevOps/AzureDevOpsIntegration.cs
@@ -1,7 +1,6 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 using AccessibilityInsights.Extensions.AzureDevOps.Enums;
-using AccessibilityInsights.Extensions.AzureDevOps.FileIssue;
 using AccessibilityInsights.Extensions.AzureDevOps.Models;
 using AccessibilityInsights.Extensions.Helpers;
 using Microsoft.TeamFoundation.Core.WebApi;
