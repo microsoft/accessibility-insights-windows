c420acbc706232d4d196782d1d2751c52ad0d9cf
John Alkire <4615491+jalkire@users.noreply.github.com>
2020-08-26 10:20:16 -0700

    Fix ui tests (#901)
    Currently, there are two issues with our UI tests:
    
    The live mode UI tests fail because axe-windows has been updated and reports a different number of results and because we changed our directory structure.
    The automation to load test and event files fails on my machine (but not the remote build).
    This PR addresses 1 by updating the expected result count and WildlifeManager executable path and 2 by removing some unnecessary automation complexity.

--- src/UITests/UILibrary/LiveMode.cs
+++ src/UITests/UILibrary/LiveMode.cs
@@ -13,10 +13,9 @@ namespace UITests.UILibrary
         // These AutomationIDs came from inspecting the open file dialog with ai-win.
         // The assumption is that they are the same accross machines--if they aren't,
         // we'll need a more robust way to navigate the dialog.
-        const string OpenFileToolBarAutomationID = "1001";
         const string OpenFileFolderTextBoxAutomationID = "41477";
         const string OpenFileFileTextBoxAutomationID = "1148";
-        const string OpenFileSearchBoxAutomationID = "SearchEditBox";
+        const string OpenFileAllLocationsElementName = "All locations";
 
         public string SelectedElementText => Session.FindElementByAccessibilityId(AutomationIDs.InspectTabsElementTextBlock).Text;
 
@@ -28,19 +27,13 @@ namespace UITests.UILibrary
         public void OpenFile(string folder, string fileName)
         {
             Session.FindElementByAccessibilityId(AutomationIDs.MainWinLoadButton).Click();
-
-            // this puts focus on the toolbar, which we need for the next command to work
-            Session.FindElementByAccessibilityId(OpenFileSearchBoxAutomationID).SendKeys(Keys.Shift + Keys.Tab);
-            // this turns the toolbar into a text box
-            Session.FindElementByAccessibilityId(OpenFileToolBarAutomationID).SendKeys(Keys.Enter);
-
+            Session.FindElementByName(OpenFileAllLocationsElementName).Click();
+            
             var folderTextbox = Session.FindElementByAccessibilityId(OpenFileFolderTextBoxAutomationID);
-            folderTextbox.SendKeys(folder);
-            folderTextbox.SendKeys(Keys.Enter);
+            folderTextbox.SendKeys(folder + Keys.Enter);
 
             var fileTextbox = Session.FindElementByAccessibilityId(OpenFileFileTextBoxAutomationID);
-            fileTextbox.SendKeys(fileName);
-            fileTextbox.SendKeys(Keys.Enter);
+            fileTextbox.SendKeys(fileName + Keys.Enter);
         }
 
         public void TogglePause() => Session.FindElementByAccessibilityId(AutomationIDs.MainWindow).SendKeys(Keys.Shift + Keys.F5);
