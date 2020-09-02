fe68a340c9d27a64b9b4a31f9a8e4dcfffdfc8ec
Dave Tryon <45672944+DaveTryon@users.noreply.github.com>
2020-09-02 14:44:30 -0700

    Fix issues with accessing resources in GutHib extension

--- src/AccessibilityInsights.Extensions.GitHub/Properties/AssemblyInfo.cs
+++ src/AccessibilityInsights.Extensions.GitHub/Properties/AssemblyInfo.cs
@@ -3,6 +3,7 @@
 using System.Resources;
 using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
@@ -25,3 +26,8 @@ using System.Runtime.InteropServices;
 [assembly: Guid("82ed31c8-6f6f-4a5b-a601-ddba4c417e5e")]
 [assembly: NeutralResourcesLanguage("en-US")]
 
+#if ENABLE_SIGNING
+[assembly: InternalsVisibleTo("AccessibilityInsights.Extensions.GitHubUnitTests,PublicKey=002400000480000094000000060200000024000052534131000400000100010007d1fa57c4aed9f0a32e84aa0faefd0de9e8fd6aec8f87fb03766c834c99921eb23be79ad9d5dcc1dd9ad236132102900b723cf980957fc4e177108fc607774f29e8320e92ea05ece4e821c0a5efe8f1645c4c0c93c1ab99285d622caa652c1dfad63d745d6f2de5f17e5eaf0fc4963d261c8a12436518206dc093344d5ad293")]
+#else
+[assembly: InternalsVisibleTo("AccessibilityInsights.Extensions.GitHubUnitTests")]
+#endif
--- src/AccessibilityInsights.Extensions.GitHubUnitTests/LinkValidatorTest.cs
+++ src/AccessibilityInsights.Extensions.GitHubUnitTests/LinkValidatorTest.cs
@@ -1,10 +1,11 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using AccessibilityInsights.Extensions.GitHub;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 
-namespace AccessibilityInsights.Extensions.GitHub
+namespace AccessibilityInsights.Extensions.GitHubUnitTests
 {
     [TestClass]
     public class LinkValidatorTest
