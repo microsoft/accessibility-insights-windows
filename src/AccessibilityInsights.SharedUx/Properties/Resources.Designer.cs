82865910b5f1692f9917b128d082f979decdeebe
Dave Tryon <45672944+DaveTryon@users.noreply.github.com>
2020-09-02 10:59:52 -0700

    Revise explanatory text (design feedback)

--- src/AccessibilityInsights.SharedUx/Properties/Resources.Designer.cs
+++ src/AccessibilityInsights.SharedUx/Properties/Resources.Designer.cs
@@ -729,7 +729,7 @@ namespace AccessibilityInsights.SharedUx.Properties {
         }
 
         /// <summary>
-        ///   Looks up a localized string similar to When you enable auto-detection of color contrast ratios, you can detect color contrast ratios by hovering over an element or setting the keyboard focus on it..
+        ///   Looks up a localized string similar to Auto-detection allows you to evaulate color contrast ratios by hovering over an element or setting the keyboard focus on it..
         /// </summary>
         public static string ColorContrast_AutoDetectGuidance {
             get {
