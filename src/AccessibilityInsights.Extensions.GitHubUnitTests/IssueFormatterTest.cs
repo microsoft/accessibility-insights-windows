07ee8d46996d06594e44ee0093ef5c9997f1cd24
Dave Tryon <45672944+DaveTryon@users.noreply.github.com>
2020-09-03 07:30:57 -0700

    Fix: Unit tests should be in their own namespace (#919)

--- src/AccessibilityInsights.Extensions.GitHubUnitTests/IssueFormatterTest.cs
+++ src/AccessibilityInsights.Extensions.GitHubUnitTests/IssueFormatterTest.cs
@@ -1,11 +1,12 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using AccessibilityInsights.Extensions.GitHub;
 using AccessibilityInsights.Extensions.Interfaces.IssueReporting;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Globalization;
 
-namespace AccessibilityInsights.Extensions.GitHub
+namespace AccessibilityInsights.Extensions.GitHubUnitTests
 {
     [TestClass]
     public class IssueFormatterTest
@@ -50,7 +51,7 @@ namespace AccessibilityInsights.Extensions.GitHub
 
         private string GetLink(string link, IIssueFormatter formatter)
         {
-            string FormattedURL = string.Format(CultureInfo.InvariantCulture, Properties.Resources.FormattedLink,
+            string FormattedURL = string.Format(CultureInfo.InvariantCulture, GitHub.Properties.Resources.FormattedLink,
                link,
                formatter.GetFormattedTitle(),
                formatter.GetFormattedBody());
