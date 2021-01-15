49ae026a9ecbba3d8b11a23a4ce9c35de9c6da5f
Dave Tryon <45672944+DaveTryon@users.noreply.github.com>
2021-01-15 11:30:00 -0800

    Add Custom Action assembly with code to clean up (not yet integrated to MSI)

---
+++ src/AccessibilityInsights.CustomActions/ConfigFileCleaner.cs
@@ -0,0 +1,59 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using AccessibilityInsights.SetupLibrary;
+using Microsoft.Deployment.WindowsInstaller;
+using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace AccessibilityInsights.CustomActions
+{
+    public class ConfigFileCleaner
+    {
+        [CustomAction]
+        public static ActionResult RemoveUserConfigFiles(Session session)
+        {
+            try
+            {
+                if (IsVersionSwitcherRunning())
+                {
+                    session.Log("RemoveUserConfigFiles: Found VersionSwitcher, leaving config files intact.");
+                }
+                else
+                {
+                    DeleteConfigFiles(session);
+                }
+            }
+            catch (Exception e)
+            {
+                session.Log("Caught Exception: " + e);
+            }
+
+            return ActionResult.Success;  // Don't block installer if we can't clean up the files
+        }
+
+        private static bool IsVersionSwitcherRunning()
+        {
+            foreach (var x in Process.GetProcesses())
+            {
+                if (x.ProcessName.Equals("AccessibilityInsights.VersionSwitcher", StringComparison.OrdinalIgnoreCase))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private static void DeleteConfigFiles(Session session)
+        {
+            string configPath = FixedConfigSettingsProvider.CreateDefaultSettingsProvider().ConfigurationFolderPath;
+
+            foreach (string fileName in Directory.EnumerateFiles(configPath))
+            {
+                session.Log("RemoveUserConfigFiles: Deleting file: {0}", fileName);
+                File.Delete(fileName);
+            }
+        }
+    }
+}
