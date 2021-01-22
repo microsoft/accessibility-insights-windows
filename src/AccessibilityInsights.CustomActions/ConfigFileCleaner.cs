3be8c6eb3d6bc0cafb52c6ca513ac27595213b9c
Dave Tryon <45672944+DaveTryon@users.noreply.github.com>
2021-01-22 08:36:01 -0800

    Restore custom action
    This reverts commit bfbf5481a591292ec34561772f7569c786ceae6b.

---
+++ src/AccessibilityInsights.CustomActions/ConfigFileCleaner.cs
@@ -0,0 +1,68 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using Microsoft.Deployment.WindowsInstaller;
+using System;
+
+namespace AccessibilityInsights.CustomActions
+{
+    public class ConfigFileCleaner
+    {
+        private readonly ISystemShim _systemShim;
+
+        [CustomAction]
+        public static ActionResult RemoveUserConfigFiles(Session session)
+        {
+            ConfigFileCleaner cleaner = new ConfigFileCleaner(new SystemShim(session));
+            return cleaner.RunAction();
+        }
+
+        internal ConfigFileCleaner(ISystemShim systemShim)
+        {
+            _systemShim = systemShim;
+        }
+
+        internal ActionResult RunAction()
+        {
+            try
+            {
+                if (IsVersionSwitcherRunning())
+                {
+                    _systemShim.LogToSession("RemoveUserConfigFiles: Found VersionSwitcher, ignoring config files.");
+                }
+                else
+                {
+                    DeleteConfigFiles();
+                }
+            }
+            catch (Exception e)
+            {
+                _systemShim.LogToSession("Caught Exception: " + e);
+            }
+
+            return ActionResult.Success;  // Don't block installer if an error occurs
+        }
+
+        private bool IsVersionSwitcherRunning()
+        {
+            foreach (var processName in _systemShim.GetRunningProcessNames())
+            {
+                if (processName.Equals("AccessibilityInsights.VersionSwitcher", StringComparison.OrdinalIgnoreCase))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private void DeleteConfigFiles()
+        {
+            _systemShim.LogToSession("RemoveUserConfigFiles: Finding config files");
+            foreach (string fileName in _systemShim.GetConfigFiles())
+            {
+                _systemShim.LogToSession("RemoveUserConfigFiles: Deleting file: " + fileName);
+                _systemShim.DeleteFile(fileName);
+            }
+        }
+    }
+}
---
+++ src/AccessibilityInsights.CustomActions/SystemShim.cs
@@ -0,0 +1,48 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using AccessibilityInsights.SetupLibrary;
+using Microsoft.Deployment.WindowsInstaller;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+namespace AccessibilityInsights.CustomActions
+{
+    /// <summary>
+    /// Production implementation of ISystemShim
+    /// </summary>
+    internal class SystemShim: ISystemShim
+    {
+        private readonly Session _session;
+
+        public SystemShim(Session session)
+        {
+            _session = session;
+        }
+
+        public IEnumerable<string> GetRunningProcessNames()
+        {
+            foreach (Process process in Process.GetProcesses())
+            {
+                yield return process.ProcessName;
+            }
+        }
+
+        public IEnumerable<string> GetConfigFiles()
+        {
+            string configPath = FixedConfigSettingsProvider.CreateDefaultSettingsProvider().ConfigurationFolderPath;
+
+            return Directory.EnumerateFiles(configPath);
+        }
+
+        public void DeleteFile(string fileName)
+        {
+            File.Delete(fileName);
+        }
+
+        public void LogToSession(string message)
+        {
+            _session.Log(message);
+        }
+    }
+}
