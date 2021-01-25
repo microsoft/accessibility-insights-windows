105c3c9cbc82d0a286e12a0251fba9745a5263a4
Dave Tryon <45672944+DaveTryon@users.noreply.github.com>
2021-01-25 13:39:29 -0800

    Attempt to build CustomActions as a new-format project

---
+++ src/AccessibilityInsights.CustomActions2/Class1.cs
@@ -0,0 +1,9 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace AccessibilityInsights.CustomActions2
+{
+    public class Class1
+    {
+    }
+}
