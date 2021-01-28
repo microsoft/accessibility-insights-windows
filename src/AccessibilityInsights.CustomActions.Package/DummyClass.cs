cf6f407129c66bbdf9a49cb06b4e708f23078294
Dave Tryon <45672944+DaveTryon@users.noreply.github.com>
2021-01-27 17:33:12 -0800

    Make CustomActions.Package actually depend on CustomActions

--- src/AccessibilityInsights.CustomActions.Package/DummyClass.cs
+++ src/AccessibilityInsights.CustomActions.Package/DummyClass.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-namespace AccessibilityInsights.CustomActions
+namespace AccessibilityInsights.CustomActions_
 {
     /// <summary>
     /// This assembly is a placeholder that gets replaced by the project file. This class
