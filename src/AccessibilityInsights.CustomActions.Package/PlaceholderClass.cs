ee1c981f3d266502b4d9c8714c831312d298d7f5
Dave Tryon <45672944+DaveTryon@users.noreply.github.com>
2021-01-29 10:30:39 -0800

    Collapse iterations into a single commit

---
+++ src/AccessibilityInsights.CustomActions.Package/PlaceholderClass.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+// This assembly is purely a placeholder. A build step overwrites it with the
+// assembly from the AccessibilityInsights.CustomActions project. This code
+// exists purely to generate the placeholder assembly.
+namespace AccessibilityInsights.CustomActions.Package
+{
+    internal static class PlaceholderClass
+    {
+        internal static void PlaceholderMethod() { }
+    }
+}
