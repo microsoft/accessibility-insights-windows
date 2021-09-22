fda978aeb5b0f7cd95fec9839e96d17543dd80cd
karan <karansin@microsoft.com>
2021-09-21 17:38:28 -0700

    Update to fwlink and add copyright text

--- src/AccessibilityInsights.Extensions.AzureDevOps/FileIssue/WebviewRuntimeNotInstalled.xaml.cs
+++ src/AccessibilityInsights.Extensions.AzureDevOps/FileIssue/WebviewRuntimeNotInstalled.xaml.cs
@@ -1,4 +1,7 @@
-﻿using AccessibilityInsights.Extensions.Helpers;
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using AccessibilityInsights.Extensions.Helpers;
 using System;
 using System.Diagnostics;
 using System.Windows;
