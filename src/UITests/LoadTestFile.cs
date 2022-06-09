a7825f78b676999cc1bc3fc700b94405f05ca495
Dave Tryon <45672944+DaveTryon@users.noreply.github.com>
2022-06-09 15:39:58 -0700

    Add UITest to open file with framework issues

--- src/UITests/LoadTestFile.cs
+++ src/UITests/LoadTestFile.cs
@@ -7,27 +7,73 @@ using System.Reflection;
 namespace UITests
 {
     /// <summary>
-    /// Start the app, open an A11yTest file, navigate to 2 views (automated checks and UIA hierarchy), spot check loaded data, exit app
+    /// Tests around file load. Note that every TestMethod will restart ai-win, so use them sparingly.
     /// </summary>
     [TestClass]
     public class LoadTestFile : AIWinSession
     {
-        const string TestFileName = "WildlifeManagerTest.a11ytest";
+        private struct TestCase
+        {
+            public string TestFile;
+            public int? AutomatedChecks_NonFrameworkErrorCount;
+            public int? AutomatedChecks_FrameworkErrorCount;
+            public int UIATree_FailedResultsCount;
+            public int UIATree_AllResultsCount;
+        }
         readonly string TestFilePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "TestFiles");
 
-        /// <summary>
-        /// The entry point for this test scenario. Every TestMethod will restart ai-win, so
-        /// we want to use them sparingly.
-        /// </summary>
         [TestMethod]
         [TestCategory(TestCategory.NoStrongName), TestCategory(TestCategory.Integration)]
-        public void LoadTestFileTests()
+        public void LoadFileWithNoFrameworkIssues()
         {
-            driver.VerifyAccessibility(TestContext, "AutomatedChecks", 0);
-            ScanResultsInUIATreePage();
-            driver.TestMode.AutomatedChecks.ValidateAutomatedChecks(12, null);
-            driver.TestMode.AutomatedChecks.GoToAutomatedChecksElementDetails(0);
-            ValidateResultsInUIATree();
+            LoadTestEngine(new TestCase
+            {
+                TestFile = "WildlifeManagerTest.a11ytest",
+                AutomatedChecks_NonFrameworkErrorCount = 12,
+                AutomatedChecks_FrameworkErrorCount = null,
+                UIATree_FailedResultsCount = 2,
+                UIATree_AllResultsCount = 28,
+            });
+        }
+
+        [TestMethod]
+        [TestCategory(TestCategory.NoStrongName), TestCategory(TestCategory.Integration)]
+        public void LoadFileWithFrameworkIssues()
+        {
+            LoadTestEngine(new TestCase
+            {
+                TestFile = "WildlifeManagerTestWithFrameworkIssues.a11ytest",
+                AutomatedChecks_NonFrameworkErrorCount = 12,
+                AutomatedChecks_FrameworkErrorCount = 3,
+                UIATree_FailedResultsCount = 2,
+                UIATree_AllResultsCount = 30,
+            });
+        }
+
+        private void LoadTestEngine(TestCase testCase)
+        /// <summary>
+        /// Start the app, open an A11yTest file, navigate to 2 views (automated checks and UIA hierarchy), spot check loaded data, exit app
+        /// </summary>
+        {
+            Setup();
+
+            try
+            {
+                driver.GettingStarted.DismissTelemetry();
+                driver.GettingStarted.DismissStartupPage();
+                driver.ToggleHighlighter();
+
+                driver.LiveMode.OpenFile(TestFilePath, testCase.TestFile);
+                driver.VerifyAccessibility(TestContext, "AutomatedChecks", 0);
+                ScanResultsInUIATreePage();
+                driver.TestMode.AutomatedChecks.ValidateAutomatedChecks(testCase.AutomatedChecks_NonFrameworkErrorCount, testCase.AutomatedChecks_FrameworkErrorCount);
+                driver.TestMode.AutomatedChecks.GoToAutomatedChecksElementDetails(0);
+                ValidateResultsInUIATree(testCase);
+            }
+            finally
+            {
+                TearDown();
+            }
         }
 
         private void ScanResultsInUIATreePage()
@@ -38,9 +84,9 @@ namespace UITests
             driver.TestMode.ResultsInUIATree.BackToAutomatedChecks();
         }
 
-        private void ValidateFirstSelectedElement()
+        private void ValidateFirstSelectedElement(TestCase testCase)
         {
-            driver.TestMode.ResultsInUIATree.ValidateResults(2, 28);
+            driver.TestMode.ResultsInUIATree.ValidateResults(testCase.UIATree_FailedResultsCount, testCase.UIATree_AllResultsCount);
             driver.TestMode.ResultsInUIATree.SwitchToDetailsTab();
             driver.TestMode.ResultsInUIATree.ValidateDetails("InvokePattern", "Name: Ok", 3, 10);
             driver.TestMode.ResultsInUIATree.ValidateTree("pane 'Desktop 1' has failed test results in descendants.", 16);
@@ -53,25 +99,11 @@ namespace UITests
             driver.TestMode.ResultsInUIATree.ValidateResults(0, 0);
         }
 
-        private void ValidateResultsInUIATree()
+        private void ValidateResultsInUIATree(TestCase testCase)
         {
-            ValidateFirstSelectedElement();
+            ValidateFirstSelectedElement(testCase);
             driver.TestMode.ResultsInUIATree.SelectElementInTree(0);
             ValidateRootElement();
         }
-
-        [TestInitialize]
-        public void TestInitialize()
-        {
-            Setup();
-
-            driver.GettingStarted.DismissTelemetry();
-            driver.GettingStarted.DismissStartupPage();
-            driver.ToggleHighlighter();
-            driver.LiveMode.OpenFile(TestFilePath, TestFileName);
-        }
-
-        [TestCleanup]
-        public void TestCleanup() => TearDown();
     }
 }
