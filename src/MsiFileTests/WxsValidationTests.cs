24602fe803969e08916ed7dda3f2b342e1f918e3
Dave Tryon <45672944+DaveTryon@users.noreply.github.com>
2022-07-07 14:39:50 -0700

    chore: Add unit test to catch file set changes

---
+++ src/MsiFileTests/WxsValidationTests.cs
@@ -0,0 +1,104 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Xml;
+
+namespace MsiFileTests
+{
+    [TestClass]
+    public class WxsValidationTests
+    {
+        [TestMethod]
+        public void AllDropFilesAreAccountedFor()
+        {
+            // Fails if any non-pdb drop files exist without a corresponding entry in either
+            // the WXS file or the set of intentional exceptions
+            string currentFolder = Path.GetFullPath(Assembly.GetExecutingAssembly().Location);
+            string repoRoot = Path.GetFullPath(Path.Combine(currentFolder, @"..\..\..\..\.."));
+            string wxsFile = Path.GetFullPath(Path.Combine(repoRoot, @"MSI\Product.wxs"));
+            HashSet<string> productComponentExclusions = new HashSet<string>(StringComparer.OrdinalIgnoreCase)
+            {
+                "AccessibilityInsights.Extensions.DiskLoggingTelemetry.dll",
+                "Axe.Windows.Automation.dll",
+            };
+
+            CompareWxsSectionToDropPath(repoRoot, @"AccessibilityInsights\bin\release\net472", wxsFile, "ProductComponent", productComponentExclusions);
+            CompareWxsSectionToDropPath(repoRoot, @"AccessibilityInsights\bin\release\net472\IssueTemplates", wxsFile, "IssueTemplates");
+            CompareWxsSectionToDropPath(repoRoot, @"AccessibilityInsights.VersionSwitcher\bin\release\net472", wxsFile, "VersionSwitcher");
+        }
+
+        private static void CompareWxsSectionToDropPath(string repoRoot, string relativeDropPath,
+            string wxsFile, string wxsComponentId, HashSet<string> intentionalExclusions = null)
+        {
+            string dropPath = Path.Combine(repoRoot, relativeDropPath);
+            HashSet<string> filesInDropPath = GetNonSymbolFilesInPath(dropPath, intentionalExclusions);
+            HashSet<string> filesInWxsSection = GetFilesIncludedInWxsSection(repoRoot, wxsFile, wxsComponentId);
+
+            filesInDropPath.ExceptWith(filesInWxsSection);
+
+            Assert.IsFalse(filesInDropPath.Any(), $"Drop files not in \"{wxsComponentId}\" of WXS: {string.Join(", ", filesInDropPath)}");
+        }
+
+        private static HashSet<string> GetNonSymbolFilesInPath(string path, HashSet<string> intentionalExclusions)
+        {
+            HashSet<string> filesInPath = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+
+            foreach (string file in Directory.EnumerateFiles(path))
+            {
+                if (intentionalExclusions != null && !intentionalExclusions.Contains(Path.GetFileName(file)))
+                {
+                    string extension = Path.GetExtension(file);
+                    if (extension != ".pdb")
+                    {
+                        filesInPath.Add(Path.GetFullPath(file));
+                    }
+                }
+            }
+
+            return filesInPath;
+        }
+
+        private static HashSet<string> GetFilesIncludedInWxsSection(string repoRoot, string wxsFile, string componentId)
+        {
+            HashSet<string> filesInSection = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+
+            using (XmlReader reader = XmlReader.Create(wxsFile))
+            {
+                bool inCorrectSection = false;
+
+                while (reader.Read())
+                {
+                    switch (reader.NodeType)
+                    {
+                        case XmlNodeType.Element:
+                            {
+                                switch (reader.Name)
+                                {
+                                    case "Component":
+                                        string id = reader.GetAttribute("Id");
+                                        inCorrectSection = (id == componentId);
+                                        break;
+                                    case "File":
+                                        if (inCorrectSection)
+                                        {
+                                            string relativeFile = reader.GetAttribute("Source");
+                                            filesInSection.Add(repoRoot + relativeFile.Substring(2));
+                                        }
+                                        break;
+                                }
+                            }
+                            break;
+                    }
+                }
+            }
+
+            return filesInSection;
+        }
+    }
+}
