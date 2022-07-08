3a78ead0b38b0c7f23562585158bf1d9b6594e47
Dave Tryon <45672944+DaveTryon@users.noreply.github.com>
2022-07-08 09:03:21 -0700

    fix: Use ReportException from correct namespace

--- src/AccessibilityInsights.SharedUx/Highlighting/Win32SnapshotButton.cs
+++ src/AccessibilityInsights.SharedUx/Highlighting/Win32SnapshotButton.cs
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 using AccessibilityInsights.CommonUxComponents.Utilities;
-using AccessibilityInsights.Extensions.Helpers;
+using AccessibilityInsights.SharedUx.Telemetry;
 using AccessibilityInsights.SharedUx.Utilities;
 using AccessibilityInsights.Win32;
 using System;
