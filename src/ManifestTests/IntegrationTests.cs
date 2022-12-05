8a6c05053f710ccef223e5bb050a4b9250dd5ee5
Dave Tryon <45672944+DaveTryon@users.noreply.github.com>
2022-12-05 10:36:52 -0800

    feat(upgrader-security): Add integration tests for manifests (#1505)
    #### Details
    
    Add tests to catch potential manifest mismatches at build time. This is
    the "future PR" mentioned in the "Context" paragraph of #1493. These
    tests essentially execute the code that will be run on the client to
    make sure that the manifest is available and that it contains the
    expected data. Since these tests rely on the `MSI` and `Manifest`
    projects, and since these projects are excluded from debug builds, the
    `ManifestTests` project is likewise excluded from debug builds.
    
    ##### Motivation
    
    Keep manifest mechanism robust
    
    ##### Context
    
    <!-- Are there any parts that you've intentionally left out-of-scope for
    a later PR to handle? -->
    
    <!-- Were there any alternative approaches you considered? What
    tradeoffs did you consider? -->
    I wanted to add a test that would check for signing of the manifest
    assembly, but that wouldn't play very nicely with unsigned builds. The
    signed pipeline already verifies the manifest for correct signing, so it
    seemed safe to omit that test. I also had to create a way to bypass the
    `TrustVerifier` check for unsigned builds. It would be cool if this
    bypass were skipped in the `SignedRelease` build, but I wasn't sure how
    to make that work.
    
    #### Pull request checklist
    <!-- If a checklist item is not applicable to this change, write "n/a"
    in the checkbox -->
    
    - [ ] Run through of all [test
    scenarios](https://github.com/Microsoft/accessibility-insights-windows/blob/main/docs/Scenarios.md)
    completed?
    - [n/a] Does this address an existing issue? If yes, Issue# - 
    - [n/a] Includes UI changes?
    - [n/a] Run the production version of Accessibility Insights for Windows
    against a version with changes.
      - [n/a] Attach any screenshots / GIF's that are applicable.
    
    > Note: After the PR has been created, certain checks will be kicked
    off. All of these checks must pass before a merge.

---
+++ src/ManifestTests/IntegrationTests.cs
@@ -0,0 +1,81 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using AccessibilityInsights.SetupLibrary;
+using AccessibilityInsights.VersionSwitcher;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace ManifestTests
+{
+    [TestClass]
+    public class IntegrationTests
+    {
+        private static readonly string RawManifestFilePath = Path.GetFullPath(Path.Combine(@"..\..\..\..", @"bld\ReleaseInfo.json"));
+        private static readonly string SignedManifestFilePath = Path.GetFullPath(Path.Combine(@"..\..\..\..", @"Manifest\bin\Release\net48\AccessibilityInsights.Manifest.dll"));
+        private static readonly string MsiFilePath = Path.GetFullPath(Path.Combine(@"..\..\..\..", @"MSI\bin\Release\AccessibilityInsights.msi"));
+        private static readonly ChannelInfo rawInfo = FileHelpers.LoadDataFromJSON<ChannelInfo>(RawManifestFilePath);
+
+        [TestMethod]
+        public void MsiFileEists()
+        {
+            Assert.IsTrue(File.Exists(MsiFilePath), $"Can't find MSI file at {MsiFilePath}");
+        }
+
+        [TestMethod]
+        public void RawManifestFileExists()
+        {
+            Assert.IsTrue(File.Exists(RawManifestFilePath), $"Can't find raw manifest file at {RawManifestFilePath}");
+        }
+
+        [TestMethod]
+        public void EmbeddedManifestFileExists()
+        {
+            Assert.IsTrue(File.Exists(SignedManifestFilePath), $"Can't find signed manifest file at {SignedManifestFilePath}");
+        }
+
+        [TestMethod]
+        public void MsiFileSizeIsCorrectInRawManifest()
+        {
+            Assert.AreEqual(new FileInfo(MsiFilePath).Length, rawInfo.MsiSizeInBytes);
+        }
+
+        [TestMethod]
+        public void MsiSha512IsCorrectInRawManifest()
+        {
+            Assert.AreEqual(InstallationEngine.ComputeSha512(MsiFilePath), rawInfo.MsiSha512);
+        }
+
+        [TestMethod]
+        public void MinimumVersionIsNullInRawManifest()
+        {
+            Assert.IsNull(rawInfo.MinimumVersion);
+        }
+
+        [TestMethod]
+        public void EmbeddedManifestMatchesRawManifest()
+        {
+            using (Stream stream = new FileStream(SignedManifestFilePath, FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                stream.Position = 0;
+
+#if ENABLE_SIGNING
+                Func<Stream, bool> signingOverride = null;          // Enforces signing requirement
+#else
+                Func<Stream, bool> signingOverride = (_) => true;   // Bypasses signing requirement
+#endif
+                ChannelInfo signedInfo = ChannelInfoUtilities.GetChannelInfoFromSignedManifest(stream, signingOverride);
+
+                Assert.IsNotNull(signedInfo);
+                Assert.AreEqual(rawInfo.MsiSizeInBytes, signedInfo.MsiSizeInBytes);
+                Assert.AreEqual(rawInfo.MsiSha512, signedInfo.MsiSha512);
+                Assert.AreEqual(rawInfo.InstallAsset, signedInfo.InstallAsset);
+                Assert.AreEqual(rawInfo.ReleaseNotesAsset, signedInfo.ReleaseNotesAsset);
+                Assert.AreEqual(rawInfo.CurrentVersion, signedInfo.CurrentVersion);
+                Assert.AreEqual(rawInfo.ProductionMinimumVersion, signedInfo.ProductionMinimumVersion);
+                Assert.AreEqual(rawInfo.MinimumVersion, signedInfo.MinimumVersion);
+            }
+        }
+    }
+}
