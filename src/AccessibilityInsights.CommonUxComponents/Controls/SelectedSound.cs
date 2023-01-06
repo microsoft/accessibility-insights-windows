205d3942b887575f02d2dc7cbc12375846f8b26a
Dave Tryon <45672944+DaveTryon@users.noreply.github.com>
2023-01-06 10:55:59 -0800

    feat(audio-feedback): Add support for multiple sounds in ProgressRingControl

---
+++ src/AccessibilityInsights.CommonUxComponents/Controls/SelectedSound.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace AccessibilityInsights.CommonUxComponents.Controls
+{
+    /// <summary>
+    /// Identifies the sound to play if sounds are enabled
+    /// </summary>
+    public enum SelectedSound
+    {
+        NoSound, // Keep this first so it is the default value!
+        Scanner,
+    }
+}
