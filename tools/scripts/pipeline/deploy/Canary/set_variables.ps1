f4d70480cf6646a68bd8a12a987cf177e0af4ccf
Dave Tryon <45672944+DaveTryon@users.noreply.github.com>
2023-03-21 15:46:59 -0700

    chore: Move deployment scripts into build artifact (#1577)
    #### Details
    
    ADO release pipelines are limited to classic pipeline definitions, with
    no YAML support. Our current release pipelines include 5 inline
    PowerShell scripts, which are hard to discover and harder to review when
    changes are needed. After discussion as a team, we opted to add the
    scripts to the repo, publish them as build artifacts, then have the
    release pipelines execute the scripts that we consume from the build
    artifacts. This PR completes the first 2 steps, then we'll change the
    release pipelines once the artifact exists.
    
    Note that while the consumption pattern will change from inline to an
    artifact, the scripts themselves will not be changing (caveat: 2
    non-functional tweaks were made). The publishing of the `deploy_scripts`
    artifact occurs in the SignedRelease step, so it happens at the very end
    of the build. It was earlier in a test build just for the sake of
    efficiency.
    
    ##### Motivation
    
    Take advantage of code tools to manage our release pipelines.
    
    ##### Context
    
    <!-- Are there any parts that you've intentionally left out-of-scope for
    a later PR to handle? -->
    The pipeline changes will take effect after this is merged and the
    `deploy_scripts` artifact becomes available.
    
    <!-- Were there any alternative approaches you considered? What
    tradeoffs did you consider? -->
    
    #### Pull request checklist
    <!-- If a checklist item is not applicable to this change, write "n/a"
    in the checkbox -->
    
    - [ ] Run through of all [test
    scenarios](https://github.com/Microsoft/accessibility-insights-windows/blob/main/docs/Scenarios.md)
    completed?
    - [ ] Does this address an existing issue? If yes, Issue# - 
    - [ ] Includes UI changes?
    - [ ] Run the production version of Accessibility Insights for Windows
    against a version with changes.
      - [ ] Attach any screenshots / GIF's that are applicable.
    
    > Note: After the PR has been created, certain checks will be kicked
    off. All of these checks must pass before a merge.

---
+++ tools/scripts/pipeline/deploy/Canary/set_variables.ps1
@@ -0,0 +1,31 @@
+# Copyright (c) Microsoft. All rights reserved.
+# Licensed under the MIT license. See LICENSE file in the project root for full license information.
+<#
+.SYNOPSIS
+Set build variables that will be consumed by downstream scripts. It sets the following variables:
+
+    A11yInsightsMsiFile    is the relative path to the MSI file (e.g., 1.1.2227.01\AccessibilityInsights.msi)
+    A11yInsightsVersion    is the version (e.g., 1.1.2227.01) for this release
+    A11yInsightsTagName    is the associated tag name (e.g., v1.1.2227.01) for this release
+
+This script is assumed to run from $(Release.PrimaryArtifactSourceAlias)
+#>
+
+Set-StrictMode -Version Latest
+$script:ErrorActionPreference = 'Stop'
+
+$msiFolder = ".\msi"
+$a11yInsightsVersion = Get-ChildItem -Name -Directory $($msiFolder)
+$a11yInsightsTagName = "v" + $($a11yInsightsVersion)
+$a11yInsightsMsiFile = $($a11yInsightsVersion) + "\" + "AccessibilityInsights.msi"
+
+# Diagnostic use only--does NOT set environment variables
+Write-Host "Diagnostics values:"
+Write-Host ("A11yInsightsVersion = " + $($a11yInsightsVersion))
+Write-Host ("A11yInsightsTagName = " + $($a11yInsightsTagName))
+Write-Host ("A11yInsightsMsiFile= " + $($a11yInsightsMsiFile))
+
+# Set the environment variables
+Write-Host "##vso[task.setvariable variable=A11yInsightsVersion]$($a11yInsightsVersion)"
+Write-Host "##vso[task.setvariable variable=A11yInsightsTagName]$($a11yInsightsTagName)"
+Write-Host "##vso[task.setvariable variable=A11yInsightsMsiFile]$($a11yInsightsMsiFile)"
